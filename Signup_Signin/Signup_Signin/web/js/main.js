c0e842f24abd58e6461f5fbe7305b543ef712a16
Victor Marrero Zayas <victor@max12>
2025-11-13 09:08:02 +0100

    en ingles

---
+++ Signup_Signin/Signup_Signin/web/js/main.js
@@ -0,0 +1,12 @@
+ // Recuperamos los datos del usuario desde localStorage
+    const nombre = localStorage.getItem('nombre');
+    const saldo = localStorage.getItem('saldo');
+
+    if (nombre && saldo) {
+      document.getElementById('bienvenida').textContent = `Bienvenido, ${nombre}`;
+      document.getElementById('saldoUsuario').textContent = ` ${saldo} €`;
+    } else {
+      // Si no hay datos, redirigimos al login
+      window.location.href = "signin.html";
+    }
+
