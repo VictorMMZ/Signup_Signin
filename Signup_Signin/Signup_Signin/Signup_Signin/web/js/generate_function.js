84e2d8c38f0b33b9c90c14302b1bf9546675b93e
victor <victor@linux>
2026-01-14 12:14:21 +0000

    Delete funcional

---
+++ Signup_Signin/Signup_Signin/Signup_Signin/web/js/generate_function.js
@@ -0,0 +1,100 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+const SERVICE_URL = "http://localhost:8080/CRUDBankServerSide/webresources/movement/";
+let movements;
+const accountid="account/2654785441";
+const moveid="2654785441";
+/**
+ * Generator function that yields table rows
+ */
+
+
+
+// funcion para generar tablas 
+function* movementsRowGenerator(movements) {
+    for (const movement of movements) {
+           const tr = document.createElement("tr");
+           //solo los campos que queremos ver
+           ["timestamp", "amount","balance"].forEach(field => {
+                const td = document.createElement("td");
+                td.textContent = movement[field];
+                tr.appendChild(td);
+        });
+     yield tr;
+ }
+ }
+ 
+ // funcion fetch para recuperar datos del servidor parseando a XML
+    async function fetchMovements() {
+        const response = await fetch(SERVICE_URL +`${accountid}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/xml"
+              }
+         });
+        const xmlText = await response.text();
+        return parseMovementsXML(xmlText);
+    }
+    
+    //funcion fetch con la ruta del movmiento a elimnar,pasando como parametro el id al llamar a la funcion
+    async function fetchMovementsForRemove(deleteid) {
+           const response = await fetch(SERVICE_URL + `${deleteid}`, {
+            // metodo DELETE para eliminar del servidor y asi mismo de la base de datos
+            method: "DELETE",
+            headers: {
+                "Accept": "application/xml"
+              }
+         });
+      
+    }
+
+
+
+// funcion de Parseo de los datos XML
+    function parseMovementsXML(xmlText) {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+        const movements = [];
+        const movementsNodes = xmlDoc.getElementsByTagName("movement");
+            for (const movementNode of movementsNodes) {
+                movements.push({
+                  
+                    timestamp: movementNode.getElementsByTagName("timestamp")[0].textContent,
+                    amount: movementNode.getElementsByTagName("amount")[0].textContent,
+                    balance: movementNode.getElementsByTagName("balance")[0].textContent,
+                    id:movementNode.getElementsByTagName("id")[0].textContent
+                    
+                    
+                });
+             }
+        return movements;
+     }
+     
+     
+//Funcion utilizada para llamar a las demas funciones
+async function buildMovementsTable() {
+    movements = await fetchMovements();
+    tbody = document.getElementById("tbodyMovements"); 
+    const rowGenerator = movementsRowGenerator(movements);
+    for (const row of rowGenerator) {
+    tbody.appendChild(row);
+ }
+}
+
+
+//funcion accionadora del boton para eliminar el ultimo movimiento
+ function deleteLast(event){   
+     
+     // creamos la variable en la que guardaremos el id del ultimo movimiento ya que estan ordenados
+     const lastoperation=movements[movements.length-1].id;
+     //llamamos tanto a la funcion fetch de eliminar como al constuir la tabla para que se haga dinamicamente(todavia queda corregir)
+     fetchMovementsForRemove(lastoperation);
+          buildMovementsTable();
+ 
+}
+// Call on page load
+    buildMovementsTable();
--- Signup_Signin/Signup_Signin/Signup_Signin/web/js/signin.js
+++ Signup_Signin/Signup_Signin/Signup_Signin/web/js/signin.js
@@ -11,7 +11,8 @@ msgBox.style.display = 'none';
                     const signInForm=document.getElementById("signInForm");
                 
                     //Creamos un objeto RegExp para validar el email
-                 
+                       tfEmail.innerHTML="";
+                    tfPassword.innerHTML="";
                    
                     //Detenemos la propagación de eventos y la acción por defecto
                     //del formulario
@@ -22,13 +23,13 @@ msgBox.style.display = 'none';
                   document.getElementById("error_pass").innerHTML="<p> You must fill in both fields</p>";
                     return;//Validar que email and password cumplen longitud
                 }            
-            if(tfEmail.value.length>255){
+        if(tfEmail.value.length>255){
                         document.getElementById("error_usu").innerHTML="<p> The email cannot be longer than 255 characters.</p>";
                         msgBox.textContent = '';
                         msgBox.style.display = 'none';
                     return;
                 }
-            if (tfPassword.value.length>255){
+                if (tfPassword.value.length>255){
                     document.getElementById("error_pass").innerHTML="<p> The password cannot exceed 255 characters.</p>";
                     msgBox.textContent = '';
                     msgBox.style.display = 'none';
@@ -50,6 +51,8 @@ msgBox.style.display = 'none';
             //hacerlo con fecth mirar codigo 
             //progrmacion funcional,le pasas a una funcion como parametro otra funcion
 
+  
+ 
         const baseUrl = "http://localhost:8080/CRUDBankServerSide/webresources/customer/sigin/";
  
         const  newpath =baseUrl +
@@ -58,46 +61,68 @@ msgBox.style.display = 'none';
              //signInForm.submit() ;
              
              
-           fetch(newpath,
-           { method: 'GET',
-                headers: { 
-                   'Content-Type': 'application/json' 
-               }
-           }) .then(response => { 
-               if (response.status === 401) {
-                   return response.text().then(() => {
-                       throw new Error('User not found!!');
-                   });
-               } else if (response.status === 404) {
-                   return response.text().then(() => {
-                       throw new Error('too many requests!!');
-                   });
-               } else if (response.status === 500) {
-                   return response.text().then(() => {
-                       throw new Error('Server Error. Please try later!!');
-                   });
-               } else if (!response.ok) {
-                   return response.text().then(text => {
-                       throw new Error(text || 'Unexpected error!!');
-                   });
-               } return response.json(); // <-- ahora obtenemos JSON 
-               }) .then(data => { // Accedemos directamente a las propiedades del JSON
-                     const nombre = data.firstName; const id = data.id; 
-                     
-                // Guardamos en localStorage 
-                  localStorage.setItem('nombre', nombre); 
-                  localStorage.setItem('id', id); 
-
-                            //en caso de que nos de el okey
+           fetch(newpath, 
+                    {
+                        method: 'GET',
+                        headers: {
+                          'Content-Type': 'application/xml'
+                        }
+                    }).then(response => {
+                        //Procesado de respuesta error 401
+                        if (response.status===401){
+                          return response.text().then(text => {
+                            throw new Error('User not found!!');
+                          });
+                        }
+                        else if (response.status===404){
+                          return response.text().then(text => {
+                            throw new Error('too many requests!!');
+                          });
+                        }
+                        //Procesado de respuesta error 500
+                        else if (response.status===500){
+                          return response.text().then(text => {
+                            throw new Error('Server Error. Please try later!!');
+                          });
+                        }
+                        //Procesado de respuesta otro error
+                        else if (!response.ok) {
+                          return response.text().then(text => {
+                            throw new Error(text || 'Unexpected error!!');
+                          });
+                        }
+                      return response.text(); // <-- obtenemos el XML
+                      })
+                        //Procesado de respuesta OK 
+                        .then(xmlText => {
+    //en caso de que nos de el okey
                             
                             msgBox.className = 'success';
-                            msgBox.textContent = 'Customer signed in successfully!';
                             msgBox.style.display = 'block';
                             msgBox.style.color = '#ffffff';
                             msgBox.style.padding="10px";
                             msgBox.style.border="1px solid black";
                             msgBox.style.backgroundColor="lightgreen" ;
                             msgBox.style.borderRadius="10px";
+                             storeResponseXMLData(xmlText);
+                        //get customer object from storage
+                            const customerName=sessionStorage.getItem("customer.firstName");
+                        //create XML from customer's data stored
+                            const customerXML=`<customer>
+                                <city>${sessionStorage.getItem("customer.city")}</city>
+                                <email>${sessionStorage.getItem("customer.email")}</email>
+                                <firstName>${sessionStorage.getItem("customer.firstName")}</firstName>
+                                <id>${sessionStorage.getItem("customer.id")}</id>
+                                <lastName>${sessionStorage.getItem("customer.lastName")}</lastName>
+                                <middleInitial>${sessionStorage.getItem("customer.middleInitial")}</middleInitial>
+                                <password>${sessionStorage.getItem("customer.password")}</password>
+                                <phone>${sessionStorage.getItem("customer.phone")}</phone>
+                                <state>${sessionStorage.getItem("customer.state")}</state>
+                                <street>${sessionStorage.getItem("customer.street")}</street>
+                                <zip>${sessionStorage.getItem("customer.zip")}</zip>
+                                </customer>`.trim();
+                            msgBox.textContent = 'Customer signed in successfully!';
+             
                               setTimeout(() => {
                                 signInForm.submit() ;
                                  window.location.href = "main.html"; }, 2000);
@@ -118,6 +143,40 @@ msgBox.style.display = 'none';
                     );
             
             }
+            
+             function storeResponseXMLData (xmlString){
+                //Create XML parser
+                const parser = new DOMParser();
+                //Parse response XML data
+                const xmlDoc=parser.parseFromString(xmlString,"application/xml");
+                //Create Customer object with data received in response
+                const customer=new Customer(
+                    xmlDoc.getElementsByTagName("id")[0].textContent,
+                    xmlDoc.getElementsByTagName("firstName")[0].textContent,
+                    xmlDoc.getElementsByTagName("lastName")[0].textContent,
+                    xmlDoc.getElementsByTagName("middleInitial")[0].textContent,
+                    xmlDoc.getElementsByTagName("street")[0].textContent,
+                    xmlDoc.getElementsByTagName("city")[0].textContent,
+                    xmlDoc.getElementsByTagName("state")[0].textContent,
+                    xmlDoc.getElementsByTagName("zip")[0].textContent,
+                    xmlDoc.getElementsByTagName("phone")[0].textContent,
+                    xmlDoc.getElementsByTagName("email")[0].textContent,
+                    xmlDoc.getElementsByTagName("password")[0].textContent,
+                );
+                // Save data to sessionStorage
+                sessionStorage.setItem("customer.id", customer.id);
+                sessionStorage.setItem("customer.firstName", customer.firstName);
+                sessionStorage.setItem("customer.lastName", customer.lastName);
+                sessionStorage.setItem("customer.middleInitial", customer.middleInitial);
+                sessionStorage.setItem("customer.street", customer.street);
+                sessionStorage.setItem("customer.city", customer.city);
+                sessionStorage.setItem("customer.state", customer.state);
+                sessionStorage.setItem("customer.zip", customer.zip);
+                sessionStorage.setItem("customer.phone", customer.phone);
+                sessionStorage.setItem("customer.email", customer.email);
+                sessionStorage.setItem("customer.password", customer.password);
+                console.log("Customer's data for "+customer.id+" saved on session storage.");
+            }
           
 const togglePassword = document.getElementById('togglePassword');
 const passwordInput = document.getElementById('tfPassword');
@@ -127,6 +186,4 @@ togglePassword.addEventListener('click', () => {
   passwordInput.setAttribute('type', type);
 
 });
-                    
                   
-
